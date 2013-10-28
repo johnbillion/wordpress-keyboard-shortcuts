614c4b202133f2a7a66b6c0ff7d2a2670ed3b4b7
John Blackbourn <johnbillion@gmail.com>
2013-10-28 00:37:16 +0000

    Further decouple the shortcuts from their actions. Interim solution while I decide whether shortcuts should be events instead of models.

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -15,53 +15,57 @@ var KeyboardShortcutList = Backbone.Collection.extend({
 // Instantiate
 window.keyboard_shortcuts = new KeyboardShortcutList();
 
+var get_shortcut_callback = function( shortcut ) {
+
+	return function( event, combo ) {
+		event.preventDefault();
+		shortcut.get( 'callback' ).call();
+	};
+
+};
+
 // Shortcut event binder
 keyboard_shortcuts.on( 'add', function( shortcut ) {
 
 	if ( shortcut.get('global') )
-		Mousetrap.bindGlobal( shortcut.get('combo'), shortcut.get('callback'), shortcut.get('action') );
+		Mousetrap.bindGlobal( shortcut.get('combo'), get_shortcut_callback( shortcut ), shortcut.get('action') );
 	else
-		Mousetrap.bind( shortcut.get('combo'), shortcut.get('callback'), shortcut.get('action') );
+		Mousetrap.bind( shortcut.get('combo'), get_shortcut_callback( shortcut ), shortcut.get('action') );
 
 } );
 
 (function($){
 
-	if ( window.console )
-		console.log( kbs ); // @TODO remove
+	// Interface Actions. These aren't coupled to keyboard shortcuts and can be invoked independently.
+	ia = _.extend(ia,{
 
-	// Controllers for the default shortcuts. This is almost generic enough to be a general dispatcher. Just need to abstract it from the event.
-	window.interaction_dispatcher = {
+		save_item : function() {
 
-		save_item : function( event ) {
-
-			if ( ! kbs.dom.save_item )
+			if ( ! this.dom.save_item )
 				return;
-			if ( ! $( kbs.dom.save_item ).length )
+			if ( ! $( this.dom.save_item ).length )
 				return;
 
 			// @TODO is this safe? For some reason it's needed when saving a post
 			window.onbeforeunload = null;
 
-			$( kbs.dom.save_item ).submit();
-			event.preventDefault();
+			$( this.dom.save_item ).submit();
 
 		},
 
-		help : function( event ) {
+		help : function() {
 
 			// @TODO it would be awesome to record the current focus position before toggling the Help screen so
 			// we can return to it when the Help screen is toggled off. #accessibility
 
-			event.preventDefault();
 			$('#contextual-help-link').click();
 
 		},
 
-		add_new_item : function( event ) {
+		add_new_item : function() {
 
-			if ( kbs.links.add_new_item ) {
-				window.location = kbs.links.add_new_item;
+			if ( this.links.add_new_item ) {
+				window.location = this.links.add_new_item;
 			} else {
 				// @TODO this needs work:
 				$('#wp-admin-bar-new-content').addClass('hover').find('a').eq(1).focus();
@@ -69,74 +73,74 @@ keyboard_shortcuts.on( 'add', function( shortcut ) {
 
 		},
 
-		// We could do with a generic URL dispatcher method. Would need to get passed some sort of identifier for the shortcut though.
-
-		add_new_post : function( event ) {
-			window.location = kbs.links.add_new_post;
+		add_new_post : function() {
+			window.location = this.links.add_new_post;
 		},
 
-		add_new_page : function( event ) {
-			window.location = kbs.links.add_new_page;
+		add_new_page : function() {
+			window.location = this.links.add_new_page;
 		},
 
-		add_new_media : function( event ) {
-			window.location = kbs.links.add_new_media;
+		add_new_media : function() {
+			window.location = this.links.add_new_media;
 		},
 
-		add_new_user : function( event ) {
-			window.location = kbs.links.add_new_user;
+		add_new_user : function() {
+			window.location = this.links.add_new_user;
 		}
 
-	};
+	});
+
+	// Default keyboard shortcuts. Here's where we link the keyboard shortcuts to our interface actions.
 
 	// Save item (contextual)
 	keyboard_shortcuts.add({
-		description : kbs.descriptions.save_item,
 		combo       : 'meta+s',
-		callback    : interaction_dispatcher.save_item,
+		description : ia.descriptions.save_item,
+		callback    : _.bind(ia.save_item,ia),
 		global      : true
 	});
 
 	// Toggle the Help screen
 	keyboard_shortcuts.add({
-		description : kbs.descriptions.help,
 		combo       : 'meta+h',
-		callback    : interaction_dispatcher.help
+		description : ia.descriptions.help,
+		callback    : _.bind(ia.help,ia)
 	});
 
 	// Add new (contextual)
 	keyboard_shortcuts.add({
-		description : kbs.descriptions.add_new_item,
 		combo       : '+ +',
-		callback    : interaction_dispatcher.add_new_item
+		description : ia.descriptions.add_new_item,
+		callback    : _.bind(ia.add_new_item,ia)
 	});
 
 	// Add new Post
 	keyboard_shortcuts.add({
-		description : kbs.descriptions.add_new_post,
 		combo       : '+ p',
-		callback    : interaction_dispatcher.add_new_post
+		description : ia.descriptions.add_new_post,
+		callback    : _.bind(ia.add_new_post,ia)
 	});
 
 	// Add new Page
 	keyboard_shortcuts.add({
-		description : kbs.descriptions.add_new_page,
 		combo       : '+ a',
-		callback    : interaction_dispatcher.add_new_page
+		description : ia.descriptions.add_new_page,
+		callback    : _.bind(ia.add_new_page,ia)
 	});
 
 	// Add new Media
 	keyboard_shortcuts.add({
-		description : kbs.descriptions.add_new_media,
 		combo       : '+ m',
-		callback    : interaction_dispatcher.add_new_media
+		description : ia.descriptions.add_new_media,
+		callback    : _.bind(ia.add_new_media,ia)
 	});
 
 	// Add new user
 	keyboard_shortcuts.add({
-		description : kbs.descriptions.add_new_user,
 		combo       : '+ u',
-		callback    : interaction_dispatcher.add_new_user
+		description : ia.descriptions.add_new_user,
+		callback    : _.bind(ia.add_new_user,ia)
 	});
 
 }(jQuery));
